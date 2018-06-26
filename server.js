ff1d825f745056a409369c956bf5aef1f5557695
Brandon Graham <bgraham452@gmail.com>
2018-06-26 12:24:59 -0400

    Created config.js module

--- server.js
+++ server.js
@@ -1,8 +1,55 @@
 'use strict';
 
+const express = require('express');
+
 // Load array of notes
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+// Create an Express application
+const app = express();
 
-// INSERT EXPRESS APP CODE HERE...
+// Create a static webserver
+app.use(express.static('public'));
+
+
+//Create PORT, which requires config.js
+const { PORT } = require('./config');
+
+// Get All (and search by query)
+app.get('/api/notes', (req, res) => {
+
+  // Basic JSON response (data is an array of objects)
+  // res.json(data);
+
+  const searchTerm = req.query.searchTerm;
+  if (searchTerm) {
+    let filteredList = data.filter(function(item) {
+      return item.title.includes(searchTerm);
+    });
+    res.json(filteredList);
+  } else {
+    res.json(data);
+  }
+
+
+});
+
+// Get a single item
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+
+  
+  let note = data.find(function(item) {
+    return item.id === Number(id);
+  });
+  res.json(note);
+
+
+});
+
+// Listen for incoming connections
+app.listen(PORT, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
